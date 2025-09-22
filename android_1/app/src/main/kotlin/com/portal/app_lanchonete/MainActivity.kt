752874f73c080850524e67b8464d769ad84e5764
cachopaweb <cachopaweb@gmail.com>
2025-09-22 14:42:46 -0400

    update

--- android_1/app/src/main/kotlin/com/portal/app_lanchonete/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.portal.app_lanchonete
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- android_2/app/src/main/kotlin/com/portal/lanchonetes/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.portal.lanchonetes
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
